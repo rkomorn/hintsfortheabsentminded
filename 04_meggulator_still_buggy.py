8f6825fe4376b165583446a04711d489d9543dcc
Romain Komorn <rkomorn@dofc.org>
2019-06-20 04:07:12 +0000

    fix 04

--- 04_meggulator_still_buggy.py
+++ 04_meggulator_still_buggy.py
@@ -4,7 +4,7 @@ from typing import Dict
 
 
 class LectureUpdate:
-    def __init__(self, author: str, days_to_lecture: int) -> None:
+    def __init__(self, author: str, days_to_lecture: int):
         self.author: str = author
         self.days_to_lecture: int = days_to_lecture
 
@@ -13,10 +13,10 @@ class Meggie:
     name: str = "Meggie"
     base_coffee_need: int = 2
 
-    def __init__(self, time_in: int) -> None:
+    def __init__(self, time_in: int):
         self.time_in: int = time_in
 
-    def compute_puns(self, puns: Dict[str, str]) -> None:
+    def compute_puns(self, puns: Dict[str, str]):
         pun_factor: int = 1
         pun_cost: int = 0
         for punster, pun in puns.items():
@@ -26,13 +26,13 @@ class Meggie:
 
         self.pun_cost = pun_cost
 
-    def compute_lecture_updates(self, lecture_update: LectureUpdate) -> None:
+    def compute_lecture_updates(self, lecture_update: LectureUpdate):
         lecture_cost: float = 2 / (lecture_update.days_to_lecture + 1)
         if lecture_update.author == "Romain":
             lecture_cost += 1
         self.lecture_cost = lecture_cost
 
-    def compute_day_of_week(self, date: datetime) -> float:
+    def compute_day_of_week(self, date):
         day = date.weekday()
         if day > 5:
             return 0
@@ -40,8 +40,8 @@ class Meggie:
             day_cost = day / 1.5
             self.day_cost = day_cost
 
-    def meggulate(self) -> float:
-        coffee_needed: float = float(self.base_coffee_need)
+    def meggulate(self):
+        coffee_needed = self.base_coffee_need
         coffee_needed += max(10 - self.time_in, 0) * 0.75
         coffee_needed += self.pun_cost * 0.01
         coffee_needed += self.lecture_cost
@@ -59,6 +59,6 @@ meggie.compute_puns(
 )
 meggie.compute_lecture_updates(LectureUpdate("Romain", 2))
 coffee_needed = meggie.meggulate()
-coffee_needed += meggie.compute_day_of_week("06/21/2019")
+coffee_needed += meggie.compute_day_of_week(datetime.today())
 coffee_needed = round(coffee_needed, 1)
 print(f"{meggie.name} needs {coffee_needed} cups of coffee.")
