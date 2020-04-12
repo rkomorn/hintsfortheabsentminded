08279f2d85cfa16221d8d95a217677600d9f4298
Romain Komorn <romain.komorn@hackbrightacademy.com>
2020-04-12 15:53:42 -0700

    Better black before

--- black_before.py
+++ black_before.py
@@ -2,7 +2,8 @@ from random import randint
 
 
 def splurge(customer_id, customer_name, num_coffee_cups, coffee_shop_name, total_amount):
-    print("{} ({}) ordered {} cups of coffee at {} for a total of ${}".format(customer_name, customer_id, num_coffee_cups, coffee_shop_name, total_price * randint(0,2)))
+    print("{} ({}) ordered {} cups of coffee at {} for a total of ${}".format(
+        customer_name, customer_id, num_coffee_cups, coffee_shop_name, total_price * randint(0,2)))
 def another_function():
     pass
 
