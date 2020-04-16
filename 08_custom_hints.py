38308f152990cabb26c866ad225a81996c27c343
Romain Komorn <romain.komorn@hackbrightacademy.com>
2020-04-15 22:53:47 -0700

    Simplify custom hints

--- 08_custom_hints.py
+++ 08_custom_hints.py
@@ -2,6 +2,6 @@ from typing import Dict, List, Tuple
 
 MyCustomType = Dict[str, Dict[int, List[Tuple[int, int, str]]]]
 
-good_wtf: MyCustomType = {"1": {2: [(3, 4, "5")]}}
+good: MyCustomType = {"1": {2: [(3, 4, "5")]}}
 
-bad_wtf: MyCustomType = {"1": {"2": [("3", 4, 5)]}}
+bad: MyCustomType = {"1": {"2": [("3", 4, 5)]}}
